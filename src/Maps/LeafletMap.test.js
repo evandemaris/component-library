bfd0603cd08d19ae088bf417e8613ce4c7f11424
Evan Demaris <Evan Demaris>
2017-02-08 20:38:18 -0800

    updated testing

--- src/Maps/LeafletMap.test.js
+++ src/Maps/LeafletMap.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Button from './Leaflet';
+import Leaflet from './LeafletMap';
 
 describe('Leaflet', () => {
   const wrapper = shallow(<Leaflet />);
