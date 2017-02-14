fe25f12e26fd119757952e7e5a76aa626d32dafe
evandemaris <evandemaris@gmail.com>
2017-02-13 19:08:02 -0800

    geojson added

--- src/Maps/LeafletMap.js
+++ src/Maps/LeafletMap.js
@@ -6,9 +6,8 @@ import data from './data.json';
 
 const cx = classNames.bind(styles);
 const className = cx({ base: true });
-require('../../assets/leaflet.css');
 
-const markers = [
+const markers = [/*
   {
     position: { lng: -122.673447, lat: 45.5225581 },
     text: 'Voodoo Doughnut',
@@ -20,7 +19,7 @@ const markers = [
   {
     position: { lng: -122.67535700000002, lat: 45.5192743 },
     text: 'Barista'
-  }
+  }*/
 ];
 
 const MakeMarker = ({ map, pos }) => (
@@ -35,13 +34,6 @@ class Leaflet extends React.Component {
   constructor() {
     super();
     this.state = {
-      geoJson:{
-        "type": "FeatureCollection",
-        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-        "features": [
-        { "type": "Feature", "properties": { "NAME": "Test point", "URL": "http:\/\/www.google.com\/", "LINE": "F-G" }, "geometry": { "type": "Point", "coordinates": [ 45.52, -122.67 ] } },
-        ]
-      },
       lat: 45.5236111,
       lng: -122.675,
       zoom: 11,
